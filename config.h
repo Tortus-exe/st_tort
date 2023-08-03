55fa86b57b21b05b1d2613e867c99ef427cc9038
Tortus-exe <65506144+Tortus-exe@users.noreply.github.com>
2023-08-02 18:48:57 -0700

    change config slightly so it's more visible

--- config.h
+++ config.h
@@ -5,9 +5,9 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Fira Code Nerd Tort:pixelsize=25:antialias=true:autohint=true";
+static char *font = "Fira Code Nerd Tort:pixelsize=30:antialias=true:autohint=true";
 static char *font2[] = {
-	"Fira Code APL:pixelsize=25:antialias=true:autohint=true",
+	"Fira Code APL:pixelsize=30:antialias=true:autohint=true",
 /*	"Hack Nerd Font Mono:pixelsize=11:antialias=true:autohint=true", */
 };
 static int borderpx = 2;
@@ -110,7 +110,7 @@ char *termname = "st-256color";
 unsigned int tabspaces = 8;
 
 /* bg opacity */
-float alpha = 0.8;
+float alpha = 0.85;
 
 /* Terminal colors (16 first used in escape sequence) */
 static const char *colorname[] = {
